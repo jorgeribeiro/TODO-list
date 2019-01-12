776845adf47ad18714a622eb6c871408d6caa82c
Jorge Ribeiro <joorgemelo@gmail.com>
2019-01-12 09:28:12 -0300

    Controller e model para Task criados

---
+++ api/app/Http/Controllers/TasksController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        return Task::findOrFail($id);
+    }
+
+}
