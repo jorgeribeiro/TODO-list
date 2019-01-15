008f26b9b35f1bd0803ac45444c1bc7b5c59c398
Jorge Ribeiro <joorgemelo@gmail.com>
2019-01-15 19:23:14 -0300

    Seeder implementado

---
+++ api/database/seeds/TasksTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tasks')->insert([
+            'titulo' => str_random(10),
+            'descricao' => str_random(30)
+        ]);
+    }
+}
