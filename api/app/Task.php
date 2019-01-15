61729aae13d32ff7e049612bd1ea2326bba97ece
Jorge Ribeiro <joorgemelo@gmail.com>
2019-01-15 19:12:36 -0300

    Pequenas modificações

--- api/app/Task.php
+++ api/app/Task.php
@@ -2,15 +2,10 @@
 
 namespace App;
 
-use Illuminate\Auth\Authenticatable;
-use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class Task extends Model
 {
-    use Authenticatable, Authorizable;
 
     /**
      * The attributes that are mass assignable.
