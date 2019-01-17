967cd90b22ee8c16441b6db031475f9425c4687b
Jorge Ribeiro <joorgemelo@gmail.com>
2019-01-17 18:10:33 -0300

    Axios GET implementado. Lista de tasks ainda não visível (?)

--- app/src/components/Tasks.vue
+++ app/src/components/Tasks.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ d.titulo }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'Tasks',
+
+  data() {
+    return {
+      tasks: {}
+    }
+  },
+
+  mounted() {
+    axios
+      .get('http://localhost:8000/tasks')
+      .then(response => (this.tasks = response))
   }
 }
 </script>
