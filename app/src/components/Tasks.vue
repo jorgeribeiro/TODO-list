9f8d9f041f2049958ce8d025d172f9b992b70c22
Jorge Ribeiro <joorgemelo@gmail.com>
2019-01-22 20:28:07 -0300

    Lista funcionando (finalmente)!

--- app/src/components/Tasks.vue
+++ app/src/components/Tasks.vue
@@ -1,10 +1,5 @@
 <template>
-  <div class="hello">
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
+  <div>   
     <ul v-for="task in tasks" :key="task.id">
       <li>
         {{ task.title }} - {{ task.description }}
